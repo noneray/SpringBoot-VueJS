5f5683bf040710a977b8bf7ddd91acfaf359cc34
noneray <noneray@outlook.com>
2022-05-02 00:17:02 +0800

    移除Mock服务

--- vue-element-admin/vue.config.js
+++ vue-element-admin/vue.config.js
@@ -46,8 +46,8 @@ module.exports = {
     overlay: {
       warnings: false,
       errors: true
-    },
-    before: require('./mock/mock-server.js')
+    }
+    // after: require('./mock/mock-server.js')
   },
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
